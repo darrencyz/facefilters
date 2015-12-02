6f87b0f9318e37336aa249cecc10bc287b8ada1a
Peter Chu <cgpchu@uwaterloo.ca>
2015-12-02 11:11:51 -0500

    add routes

---
+++ runserver.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/pynderbot', methods=['POST'])
+def pynderbot():
+    return render_template("pynderbot.html", userId = request.form['userId'], name = request.form['name'], accessToken = request.form['accessToken'])
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
