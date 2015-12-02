343f200bd21fca1fb8be2cfbba95fe2682406861
Peter Chu <cgpchu@uwaterloo.ca>
2015-12-02 16:32:24 -0500

    add access token

--- static/pynder_auth.js
+++ static/pynder_auth.js
@@ -33,27 +33,49 @@ function checkLoginState() {
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '494726047365676',
-        cookie     : true,  // enable cookies to allow the server to access 
-        // the session
-        xfbml      : true,  // parse social plugins on this page
-        version    : 'v2.2' // use version 2.2
+    cookie     : true,  // enable cookies to allow the server to access 
+    // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.2' // use version 2.2
+    });
+
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
     });
 };
 
 // Load the SDK asynchronously
 (function(d, s, id) {
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) return;
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
 function authenticate(response) {
-    var url = "http://localhost:5000/pynderbot";
-    jQuery.post(url, {userId: response.userID, accessToken: response.accessToken }, function(response) {
-        document.write(response);
-    });
+    var access_token = FB.getAuthResponse()['accessToken']
+        FB.api('/me', function(response2) {
+            var url = "http://localhost:5000/pynderbot";
+            jQuery.post(url, {userId: response.userID, accessToken: access_token, name: response2.name }, function(response) {
+                document.write(response);
+            });
+        });
+
 }
+
+
